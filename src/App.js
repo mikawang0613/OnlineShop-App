2ad8f102eb3d822f7ab3a9d90341f940927e23d7
Mika <zhewang0613@gmail.com>
2021-01-13 13:24:11 -0800

    implemented firebase utils, include ability to store user authenticated info to firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import "./App.css"
 
-import {auth} from "./firebase/firebase.utils";
+import {auth,createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(){
@@ -19,11 +19,27 @@ class App extends React.Component {
 
   unsubscibeFromAuth = null
 
-  componentDidMount(){
-    this.unsubscibeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user})
-    })
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
+
 
   componentWillUnmount(){
     this.unsubscibeFromAuth();
